b1bd70fc9ae113a9f611ce77f78d6ec08641a418
NoOne <abdul.hannan13300@gmail.com>
2023-08-18 08:43:05 +0530

    Add Property Model 3

--- client/src/components/AddPropertyModal/AddPropertyModal.jsx
+++ client/src/components/AddPropertyModal/AddPropertyModal.jsx
@@ -3,73 +3,80 @@ import React, { useState } from "react";
 import AddLocation from "../AddLocation/AddLocation";
 import { useAuth0 } from "@auth0/auth0-react";
 import UploadImage from "../UploadImage/UploadImage";
+import BasicDetails from "../BasicDetails/BasicDetails";
 
 const AddPropertyModal = ({ opened, setOpened }) => {
-   const [active, setActive] = useState(0);
-   const { user } = useAuth0();
-   const [propertyDetails, setPropertyDetails] = useState({
-      title: "",
-      description: "",
-      price: 0,
-      country: "",
-      city: "",
-      address: "",
-      image: null,
-      facilities: {
-         bedrooms: 0,
-         parkings: 0,
-         bathrooms: 0,
-      },
-      userEmail: user?.email,
-   });
+  const [active, setActive] = useState(0);
+  const { user } = useAuth0();
+  const [propertyDetails, setPropertyDetails] = useState({
+    title: "",
+    description: "",
+    price: 0,
+    country: "",
+    city: "",
+    address: "",
+    image: null,
+    facilities: {
+      bedrooms: 0,
+      parkings: 0,
+      bathrooms: 0,
+    },
+    userEmail: user?.email,
+  });
 
-   const nextStep = () => {
-      setActive((current) => (current < 4 ? current + 1 : current));
-   };
+  const nextStep = () => {
+    setActive((current) => (current < 4 ? current + 1 : current));
+  };
 
-   const prevStep = () => {
-      setActive((current) => (current > 0 ? current - 1 : current));
-   };
+  const prevStep = () => {
+    setActive((current) => (current > 0 ? current - 1 : current));
+  };
 
-   return (
-      <Modal
-         opened={opened}
-         onClose={() => setOpened(false)}
-         closeOnClickOutside
-         size={"90rem"}
-      >
-         <Container h={"40rem"} w={"100%"}>
-            <Stepper
-               active={active}
-               onStepClick={setActive}
-               breakpoint="sm"
-               allowNextStepsSelect={false}
-            >
-               <Stepper.Step label="Location" description="Address">
-                  <AddLocation
-                     nextStep={nextStep}
-                     propertyDetails={propertyDetails}
-                     setPropertyDetails={setPropertyDetails}
-                  />
-               </Stepper.Step>
-               <Stepper.Step label="Second step" description="Verify email">
-                  <UploadImage
-                     prevStep={prevStep}
-                     nextStep={nextStep}
-                     propertyDetails={propertyDetails}
-                     setPropertyDetails={setPropertyDetails}
-                  />
-               </Stepper.Step>
-               <Stepper.Step label="Final step" description="Get full access">
-                  Step 3 content: Get full access
-               </Stepper.Step>
-               <Stepper.Completed>
-                  Completed, click back button to get to previous step
-               </Stepper.Completed>
-            </Stepper>
-         </Container>
-      </Modal>
-   );
+  return (
+    <Modal
+      opened={opened}
+      onClose={() => setOpened(false)}
+      closeOnClickOutside
+      size={"90rem"}
+    >
+      <Container h={"40rem"} w={"100%"}>
+        <Stepper
+          active={active}
+          onStepClick={setActive}
+          breakpoint="sm"
+          allowNextStepsSelect={false}
+        >
+          <Stepper.Step label="Location" description="Address">
+            <AddLocation
+              nextStep={nextStep}
+              propertyDetails={propertyDetails}
+              setPropertyDetails={setPropertyDetails}
+            />
+          </Stepper.Step>
+          <Stepper.Step label="Images" description="Upload">
+            <UploadImage
+              prevStep={prevStep}
+              nextStep={nextStep}
+              propertyDetails={propertyDetails}
+              setPropertyDetails={setPropertyDetails}
+            />
+          </Stepper.Step>
+          <Stepper.Step label="Final step" description="Get full access">
+            <BasicDetails
+              ploadImage
+              prevStep={prevStep}
+              nextStep={nextStep}
+              propertyDetails={propertyDetails}
+              setPropertyDetails={setPropertyDetails}
+            />
+          </Stepper.Step>
+          <Stepper.Completed>
+            Completed, click back button to get to previous step
+          </Stepper.Completed>
+        </Stepper>
+      </Container>
+    </Modal>
+  );
 };
 
 export default AddPropertyModal;
--- client/src/components/UploadImage/UploadImage.jsx
+++ client/src/components/UploadImage/UploadImage.jsx
@@ -1,55 +1,69 @@
 import React, { useEffect, useRef, useState } from "react";
 import { AiOutlineCloudUpload } from "react-icons/ai";
 import "./UploadImage.css";
+import { Button, Group } from "@mantine/core";
 
 const UploadImage = ({
-   propertyDetails,
-   setPropertyDetails,
-   nextStep,
-   prevStep,
+  propertyDetails,
+  setPropertyDetails,
+  nextStep,
+  prevStep,
 }) => {
-   const [imageURL, setImageURL] = useState(propertyDetails.image);
+  const [imageURL, setImageURL] = useState(propertyDetails.image);
 
-   //Uploading the Image to Cloudinary
-   const cloudinaryRef = useRef();
-   const widgetRef = useRef();
+  //Uploading the Image to Cloudinary
+  const cloudinaryRef = useRef();
+  const widgetRef = useRef();
 
-   useEffect(() => {
-      cloudinaryRef.current = window.cloudinary;
-      widgetRef.current = cloudinaryRef.current.createUploadWidget(
-         {
-            cloudName: "df5egkhd1",
-            uploadPreset: "qtnyo7n0",
-            maxFiles: 1,
-         },
-         (err, result) => {
-            if (result.event === "success") {
-               setImageURL(result.info.secure_url);
-            }
-         }
-      );
-   });
+  const handleNext = () => {
+    setPropertyDetails((prev) => ({ ...prev, image: imageURL }));
+    nextStep();
+  };
 
-   return (
-      <div className="flexCenter uploadWrapper">
-         {!imageURL ? (
-            <div
-               className="flexColCenter uploadZone"
-               onClick={() => widgetRef.current?.open()}
-            >
-               <AiOutlineCloudUpload size={50} color="grey" />
-               <span>Upload Image</span>
-            </div>
-         ) : (
-            <div
-               className="uploadedImage"
-               onClick={() => widgetRef.current?.open()}
-            >
-               <img src={imageURL} alt="" />
-            </div>
-         )}
-      </div>
-   );
+  useEffect(() => {
+    cloudinaryRef.current = window.cloudinary;
+    widgetRef.current = cloudinaryRef.current.createUploadWidget(
+      {
+        cloudName: "df5egkhd1",
+        uploadPreset: "qtnyo7n0",
+        maxFiles: 1,
+      },
+      (err, result) => {
+        if (result.event === "success") {
+          setImageURL(result.info.secure_url);
+        }
+      }
+    );
+  }, []);
+
+  return (
+    <div className="flexCenter uploadWrapper">
+      {!imageURL ? (
+        <div
+          className="flexColCenter uploadZone"
+          onClick={() => widgetRef.current?.open()}
+        >
+          <AiOutlineCloudUpload size={50} color="grey" />
+          <span>Upload Image</span>
+        </div>
+      ) : (
+        <div
+          className="uploadedImage"
+          onClick={() => widgetRef.current?.open()}
+        >
+          <img src={imageURL} alt="" />
+        </div>
+      )}
+      <Group position="center" mt={"xl"}>
+        <Button variant="default" onClick={prevStep}>
+          Back
+        </Button>
+        <Button onClick={handleNext} disabled={!imageURL}>
+          Next
+        </Button>
+      </Group>
+    </div>
+  );
 };
 
 export default UploadImage;
