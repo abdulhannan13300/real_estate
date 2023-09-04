2c70bb9f2127ba17192515aeed2925cb893c4888
Abdul Hannan <abdul.hannan13300@gmail.com>
2023-09-04 15:55:13 +0530

    Logo

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -9,67 +9,73 @@ import AddPropertyModal from "../AddPropertyModal/AddPropertyModal";
 import useAuthCheck from "../../Hooks/useAuthCheck";
 
 const Header = () => {
-  const [menuOpened, setMenuOpened] = useState(false);
-  const [modalOpened, setModalOpened] = useState(false);
+   const [menuOpened, setMenuOpened] = useState(false);
+   const [modalOpened, setModalOpened] = useState(false);
 
-  const { validateLogin } = useAuthCheck();
-  //user detail: Login logout..
-  const { loginWithRedirect, isAuthenticated, user, logout } = useAuth0();
+   const { validateLogin } = useAuthCheck();
+   //user detail: Login logout..
+   const { loginWithRedirect, isAuthenticated, user, logout } = useAuth0();
 
-  const handleAddProperty = () => {
-    if (validateLogin()) {
-      setModalOpened(true);
-    }
-  };
+   const handleAddProperty = () => {
+      if (validateLogin()) {
+         setModalOpened(true);
+      }
+   };
 
-  const getMenuStyles = (menuOpened) => {
-    if (document.documentElement.clientWidth <= 800) {
-      return { right: !menuOpened && "-100%" };
-    }
-  };
+   const getMenuStyles = (menuOpened) => {
+      if (document.documentElement.clientWidth <= 800) {
+         return { right: !menuOpened && "-100%" };
+      }
+   };
 
-  return (
-    <section className="h-wrapper">
-      <div className=" flexCenter paddings innerWidth h-container">
-        {/* logo */}
-        <Link to="/">
-          <img src="./logo.png" alt="logo" width={100} />
-        </Link>
-        <OutsideClickHandler
-          onOutsideClick={() => {
-            setMenuOpened(false);
-          }}
-        >
-          <div className="flexCenter h-menu" style={getMenuStyles(menuOpened)}>
-            <NavLink to={"/properties"}>Properties</NavLink>
+   return (
+      <section className="h-wrapper">
+         <div className=" flexCenter paddings innerWidth h-container">
+            {/* logo */}
+            <Link to="/">
+               <img src="./logo-1.png" alt="logo" width={100} />
+            </Link>
+            <OutsideClickHandler
+               onOutsideClick={() => {
+                  setMenuOpened(false);
+               }}
+            >
+               <div
+                  className="flexCenter h-menu"
+                  style={getMenuStyles(menuOpened)}
+               >
+                  <NavLink to={"/properties"}>Properties</NavLink>
 
-            <a href="mailto:abdul.hannan13300@gmail.com">Contact</a>
+                  <a href="mailto:abdul.hannan13300@gmail.com">Contact</a>
 
-            {/* Add property model */}
-            <div onClick={handleAddProperty}>Add Property</div>
-            <AddPropertyModal opened={modalOpened} setOpened={setModalOpened} />
+                  {/* Add property model */}
+                  <div onClick={handleAddProperty}>Add Property</div>
+                  <AddPropertyModal
+                     opened={modalOpened}
+                     setOpened={setModalOpened}
+                  />
 
-            {/* login button */}
-            {!isAuthenticated ? (
-              <button className="button" onClick={loginWithRedirect}>
-                Login
-              </button>
-            ) : (
-              <ProfileMenu user={user} logout={logout} />
-            )}
-          </div>
-        </OutsideClickHandler>
+                  {/* login button */}
+                  {!isAuthenticated ? (
+                     <button className="button" onClick={loginWithRedirect}>
+                        Login
+                     </button>
+                  ) : (
+                     <ProfileMenu user={user} logout={logout} />
+                  )}
+               </div>
+            </OutsideClickHandler>
 
-        {/* for medium and small screens */}
-        <div
-          className="menu-icon"
-          onClick={() => setMenuOpened((prev) => !prev)}
-        >
-          <BiMenuAltRight size={30} />
-        </div>
-      </div>
-    </section>
-  );
+            {/* for medium and small screens */}
+            <div
+               className="menu-icon"
+               onClick={() => setMenuOpened((prev) => !prev)}
+            >
+               <BiMenuAltRight size={30} />
+            </div>
+         </div>
+      </section>
+   );
 };
 
 export default Header;
