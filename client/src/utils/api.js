e9925b7e78f0bb2a8a7833bd054bc8b739691499
NoOne <71642636+abdulhannan13300@users.noreply.github.com>
2023-09-04 16:19:19 +0530

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,7 @@ import dayjs from "dayjs";
 import { toast } from "react-toastify";
 
 export const api = axios.create({
-   baseURL: "https://homz-backend.vercel.app/",
+   baseURL: "https://homz-backend.vercel.app/api",
 });
 
 export const getAllProperties = async () => {
